63b55b2d0403d7bb8a18cd230ba5909afb78f91a
enzobarbi <82882174+enzobarbi@users.noreply.github.com>
2022-04-18 08:45:55 -0300

    Create funcoes.sql

---
+++ banco_de_dados/funcoes.sql
@@ -0,0 +1,9 @@
+select position('m' in 'macaco')
+
+select round(avg (ano_nasc)) from animais 
+
+select GREATEST (valor) from editora 
+
+select Least (valor) from editora order by valor
+
+select sum (valor) from editora
